8962bd97c5ea0557189e3c76e1837e22c9ba61c5
Ivan Kh <ihvorostinin@gmail.com>
2025-07-23 19:23:19 +0700

    String value wrapper

---
+++ Sources/Value.swift
@@ -0,0 +1,38 @@
+//  Created by Ivan Khvorostinin on 21.07.2025.
+
+import Foundation
+
+public protocol StringVar {
+    var value: String { get set }
+}
+
+private extension String {
+    class EmptyVar: StringVar {
+        var value: String {
+            get { "" }
+            set {}
+        }
+    }
+    
+    class StringBlock: StringVar {
+        let get: () -> String
+        let set: (String) -> Void
+        
+        init(get: @escaping () -> String, set: @escaping (String) -> Void) {
+            self.get = get
+            self.set = set
+        }
+        
+        var value: String {
+            get { get() }
+            set { set(newValue) }
+        }
+    }
+}
+
+public extension String {
+    static let emptyVar: StringVar = EmptyVar()
+    static func makeVar(get: @escaping () -> String, set: @escaping (String) -> Void) -> StringVar {
+        StringBlock(get: get, set: set)
+    }
+}
